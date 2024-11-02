20a8e9d398dfc94473d165c1be0faba65d2e9842
ejim favour <favourejim56@gmail.com>
2024-11-02 18:00:59 +0100

    fixing migration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,18 +1,18 @@
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 
 config();
 
-const configService = new ConfigService();
+// const configService = new ConfigService();
 
 export default new DataSource({
   type: 'postgres',
-  port: +configService.get('database.port'),
-  username: configService.get('database.user'),
-  password: configService.get('database.password'),
-  host: configService.get('database.host'),
-  database: configService.get('database.name'),
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   synchronize: false,
